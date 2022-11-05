954602e3ab97f9e6ae53763e224b54780fa99a89
Victor Lobato <victorlobato2310@gmail.com>
2022-11-05 14:54:19 -0300

    feat - adicionando rota buscaEmailOuUsuario

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import cadastraUsuarioRouter from "./cadastraUsuario.routes";
+import buscaEmailOuUsuarioRouter from "./buscaEmailOuUsuario.routes";
+import cadastraContaRouter from "./cadastraConta.routes";
 
 const router = Router();
 const rotaPadrao = '/api/v1';
 
-router.use(rotaPadrao, cadastraUsuarioRouter);
+router.use(rotaPadrao, cadastraContaRouter);
+router.use(rotaPadrao, buscaEmailOuUsuarioRouter);
 
 export default router;
