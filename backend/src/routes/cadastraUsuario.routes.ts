9e8856a537bc2b1c62154523465f83072c2546ef
Victor Lobato <victorlobato2310@gmail.com>
2022-11-05 14:49:57 -0300

    feat - adicionando validaCampos na rota

--- backend/src/routes/cadastraUsuario.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import cadastraUsuarioController from "../controllers/cadastraUsuario.controller";
-
-const cadastraUsuarioRouter = Router();
-
-cadastraUsuarioRouter.post('/cadastraUsuario', cadastraUsuarioController.execute);
-
-export default cadastraUsuarioRouter;
