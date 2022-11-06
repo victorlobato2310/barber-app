32e5b96644077b6736403d0b531cf4ae405f81b2
Victor Lobato <victorlobato2310@gmail.com>
2022-11-06 15:33:36 -0300

    feat - adicionando interfaces buscarEmailOuUsuario

--- backend/src/repositories/buscarEmailOuUsuario.repository.ts
+++ backend/src/repositories/buscarEmailOuUsuario.repository.ts
@@ -1,14 +1,16 @@
+import { IConta } from './../interfaces/IConta';
 import { AppDataSource } from "../data-source";
 import Cliente from "../entities/Cliente.entity";
 import { Conta } from "../entities/Conta.entity";
 import AppError from "../errors/AppError";
 import { IBuscaEmailOuUsuarioRepository } from "./interfaces/IBuscaEmailOuUsuarioRepository";
+import { ICliente } from '../interfaces/ICliente';
 
 export class BuscarEmailOuUsuarioRepository implements IBuscaEmailOuUsuarioRepository {
 
     constructor(){}
 
-    async getByEmail(email: string): Promise<any> {
+    async getByEmail(email: string): Promise<ICliente | any> {
 
         try {
             const repository = AppDataSource.getRepository(Cliente);
@@ -30,7 +32,7 @@ export class BuscarEmailOuUsuarioRepository implements IBuscaEmailOuUsuarioRepos
         }
     }
 
-    async getByUsuario(usuario: string): Promise<any> {
+    async getByUsuario(usuario: string): Promise<IConta | any> {
 
         try {
             const repository = AppDataSource.getRepository(Conta);
--- backend/src/repositories/interfaces/IBuscaEmailOuUsuarioRepository.ts
+++ backend/src/repositories/interfaces/IBuscaEmailOuUsuarioRepository.ts
@@ -1,6 +1,9 @@
+import { ICliente } from './../../interfaces/ICliente';
+import { IConta } from "../../interfaces/IConta";
+
 export interface IBuscaEmailOuUsuarioRepository {
     
-    getByEmail(email: string): Promise<any>;
+    getByEmail(email: string): Promise<ICliente | any>;
 
-    getByUsuario(usuario: string) : Promise<any>;
+    getByUsuario(usuario: string) : Promise<IConta | any>;
 }
