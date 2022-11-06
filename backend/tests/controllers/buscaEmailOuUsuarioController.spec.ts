eb22cd7e2bf5540ace20038fe58852caeaa4eb44
Victor Lobato <victorlobato2310@gmail.com>
2022-11-06 16:39:44 -0300

    feat - adicionando configuracoes do jest

---
+++ backend/tests/controllers/buscaEmailOuUsuarioController.spec.ts
@@ -0,0 +1,8 @@
+import app from "../../src/server";
+const request = require("supertest");
+describe('testando buscaEmailOuUsuarioController', () => {
+    
+    test("ok", async () => {
+        expect(true).toBe(true);
+    });
+});
