07bb024404e014f3b55b38b91aff0b74d216411b
Victor Lobato <victorlobato2310@gmail.com>
2022-11-15 09:24:24 -0300

    feat - adicionando validators cadastraCliente

--- backend/src/routes/cadastraCliente.routes.ts
+++ backend/src/routes/cadastraCliente.routes.ts
@@ -1,20 +1,10 @@
 import { Router } from "express";
 import cadastraUsuarioController from "../controllers/cadastraCliente.controller";
 import { validaCampos } from "../middlewares/validaCampos.middleware";
-import { body } from "express-validator";
+import cadastraClienteValidator from "../validators/cadastraCliente.validator";
 
 const cadastraContaRouter = Router();
 
-const camposObrigatorios = [
-    body('nome').notEmpty().withMessage("O campo nome é obrigatório"),
-    body('email').notEmpty().withMessage("O campo nome é obrigatório"),
-    body('email').isEmail(),
-    body('usuario').notEmpty().withMessage("O campo nome é obrigatório"),
-    body('usuario').isLength({ min: 8 }).withMessage("O campo senha precisa ter no min 8 caracteres."),
-    body('senha').notEmpty().withMessage("O campo senha é obrigatório"),
-    body('senha').isLength({ min: 8 }).withMessage("O campo senha precisa ter no min 8 caracteres."),
-];
-
-cadastraContaRouter.post('/cadastraUsuario', camposObrigatorios, validaCampos, cadastraUsuarioController.execute);
+cadastraContaRouter.post('/cadastraCliente', cadastraClienteValidator.camposObrigatorios, validaCampos, cadastraUsuarioController.execute);
 
 export default cadastraContaRouter;
